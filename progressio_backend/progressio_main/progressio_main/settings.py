66a55a1585051393fffdf72c4000ff4231bc8e41
PRASHANT RATHOUR <112960345+Prashantrathour@users.noreply.github.com>
2023-09-07 21:29:32 +0530

    deploye

--- progressio_backend/progressio_main/progressio_main/settings.py
+++ progressio_backend/progressio_main/progressio_main/settings.py
@@ -126,21 +126,22 @@ WSGI_APPLICATION = 'progressio_main.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "data",
+        "USER": "prashant",
+        "PASSWORD": "Prashant123@",
+        "HOST": "127.0.0.1",
+        "PORT": "3306",
+    }
+}
+
 import dj_database_url
 DATABASES = {
    'default': dj_database_url.parse(env("DATA_BASE_URL"))
 }
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.mysql",
-#         "NAME": "data",
-#         "USER": "prashant",
-#         "PASSWORD": "Prashant123@",
-#         "HOST": "127.0.0.1",
-#         "PORT": "3306",
-#     }
-# }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
